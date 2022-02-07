803d86709c4d73c6f40d0236f95c6a31a89362ae
Ahsan <ahsansharif06@gmail.com>
2022-02-07 14:07:27 -0600

    added dto with lombok int project

--- src/main/java/com/ahsan/GitTestApplication.java
+++ src/main/java/com/ahsan/GitTestApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,10 +13,12 @@ public class GitTestApplication
 {
 
 	@PostMapping("/purchase/{userName}/{amount}/{productName}")
-	public String purchase(@PathVariable String userName,@PathVariable double amount, @PathVariable String productName)
+	public String purchase(@RequestBody Order order)
 	{
 		
-		return "Hi"+userName+" order for "+productName+" with amount "+amount+" stored successfully";
+		return "";
+		
+	//	return "Hi"+ order.get +" order for "+productName+" with amount "+amount+" stored successfully";
 	}
 	
 	
